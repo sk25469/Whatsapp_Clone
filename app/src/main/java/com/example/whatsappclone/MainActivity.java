98e6c156a194bff1d9e6b5532322811ec33221d2
sk25469 <sk25469@gmail.com>
2021-05-22 23:55:20 +0530

    This is the first commit adding all files

---
+++ app/src/main/java/com/example/whatsappclone/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mPhoneNumber, mVerificationCode;
+    private Button mVerifyCodeBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPhoneNumber = findViewById(R.id.phoneNumber);
+        mVerificationCode = findViewById(R.id.verificationCode);
+        mVerifyCodeBtn = findViewById(R.id.sendVerifyCodeBtn);
+    }
+}
